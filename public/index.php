ff9ba04619cb001d2c960f84766235b0fb3b83ff
Nathan Pope <ncpope@gmail.com>
2020-03-05 17:45:19 +0800

    add code pipeline yaml file and rds db config.

--- public/index.php
+++ public/index.php
@@ -55,6 +55,14 @@ $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
 );
 
+// Begin RDS Test
+if(DB::connection()->getDatabaseName()) {
+    echo "Connected to database: " . DB::connection()->getDatabaseName();
+} else {
+    echo "No database";
+}
+// End RDS Test
+
 $response->send();
 
 $kernel->terminate($request, $response);
